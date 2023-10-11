06cc247296a0d67d992800dc52f275d598c9cfcc
arimaghfiron <arimaghfiron@gmail.com>
2023-10-11 21:01:42 +0700

    Add regex validation and error handler

--- js/script.js
+++ js/script.js
@@ -7,19 +7,30 @@ const btnReverse = document.getElementById("btn-rereverse")
 const inputLabel = document.getElementById("input-label")
 const resultLabel = document.getElementById("result-label")
 let isReverse = false
+const intRegex = /^[-.,e\d]+$/
 
 const convert = () => {
-    if(!isReverse){
+  try{
+    if(intRegex.test(inputField.value)){
+      if(!isReverse){
         resultField.value = (inputField.value * 9 / 5) + 32
         formulaField.value = `${inputField.value}℃ * (9/5) + 32 = ${resultField.value}℉`
-    }
-    else{
+      }
+      else{
         resultField.value = (inputField.value - 32) * 5 / 9
         formulaField.value = `(${inputField.value}℉ - 32) * 5/9 = ${resultField.value}℃`
+      }
+    } else {
+      throw new SyntaxError("Hanya menerima input tipe angka (desimal & negatif) !")
     }
+  }
+  catch(error){
+    alert("Konversi Gagal : " + error.message)
+  }
 }
 
 const reverse = () => {
+  try {
     isReverse = !isReverse
     let temp = inputLabel.innerText 
     inputLabel.innerText = resultLabel.innerText 
@@ -28,10 +39,19 @@ const reverse = () => {
     if(resultField.value){
         convert()
     }
+  }
+  catch(error) {
+    alert("Reverse Gagal : " + error.message)
+  }
 }
 
 const reset = () => {
+  try {
     inputField.value = null
     resultField.value = ""
     formulaField.value = ""
+  }
+  catch(error) {
+    alert("Reset Gagal : " + error.message)
+  }
 }
